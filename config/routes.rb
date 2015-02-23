fd6cec43bf87809bbd988b9c5e79ebb5f908b6db
jrotolo <jrotol5@lsu.edu>
2015-02-23 16:21:55 -0600

    Basic comment feature working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :comments
+
   get 'pages/index'
 
   root to: 'visitors#index'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150222170829) do
+ActiveRecord::Schema.define(version: 20150223212810) do
+
+  create_table "comments", force: true do |t|
+    t.text     "comment"
+    t.string   "author"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
