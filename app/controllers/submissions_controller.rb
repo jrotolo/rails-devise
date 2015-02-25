7dfb02acaff9e0e017b9d911c8b4ca2ae7e289ad
jrotolo <jrotol5@lsu.edu>
2015-02-24 20:16:04 -0600

    Added drawrequests react components

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,2 +1,17 @@
 class SubmissionsController < ApplicationController
+  respond_to :json
+
+  def index
+    respond_with Submission.all
+  end
+
+  def create
+    respond_with Submission.create(submission_params)
+  end
+
+  private
+
+  def submission_params
+    params.require(:submission).permit(:path, :description)
+  end
 end
